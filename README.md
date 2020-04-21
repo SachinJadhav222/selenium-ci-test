61c6d3e1677cfc81d0977180dc90ddcde771518a
sachinjadhav222 <sachin.jadhav222@yahoo.com>
2020-04-21 15:21:53 +0100

    source code

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # selenium-ci-test
+Selenium Server/Grid Hub Start
+
+java -jar selenium-server-standalone-3.14.jar -role hub
+
+Set chromedriver on Grid
+
+java -Dwebdriver.chrome.driver=chromedriver -jar selenium-server-standalone-3.14.jar -role webdriver -hub http://192.168.0.18:4444/grid/register/ -port 5566
+
+java -Dwebdriver.chrome.driver=<file path of your chromedriver.exe> -jar <file path of your selenium standalone jar file> -role node -hub <url of your hub>eg:
+
+java -Dwebdriver.chrome.driver=chromedriver -jar selenium-server-standalone-3.141.59.jar  -role node -hub http://localhost:4444/grid/register -port 5566
+
+Firefox
+java -Dwebdriver.chrome.driver=geckodriver -jar selenium-server-standalone-3.14.jar -role webdriver -hub http://192.168.0.18:4444/grid/register/ -port 5567
---
+++ src/test/java/com/seleniumCI/test/TestClass1.java
@@ -0,0 +1,96 @@
+package com.seleniumCI.test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.Assert;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import net.rcarz.jiraclient.BasicCredentials;
+import net.rcarz.jiraclient.Field;
+import net.rcarz.jiraclient.Issue;
+import net.rcarz.jiraclient.JiraClient;
+import net.rcarz.jiraclient.JiraException;
+
+public class TestClass1 {
+	public String seleniumHub="http://192.168.0.18:4444/wd/hub";
+	//public String webUrl="https://automationtalks.com/";
+	public String webUrl="https://www.selenium.dev/";
+
+	public static WebDriver driver;
+	
+	
+	@BeforeMethod
+	public void launchDriver() throws MalformedURLException {
+		DesiredCapabilities dr = null;
+		dr = DesiredCapabilities.chrome();
+		dr.setBrowserName("chrome");
+		dr.setPlatform(Platform.MAC);
+		System.setProperty("webdriver.chrome.driver", "chromedriver");
+
+//		dr = DesiredCapabilities.firefox();
+//		dr.setBrowserName("firefox");
+//		dr.setPlatform(Platform.MAC);
+//		System.setProperty("webdriver.firefox.driver", "geckodriver");
+//
+		driver = new RemoteWebDriver(new URL(seleniumHub), dr);
+		
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().fullscreen();
+	}
+	
+	@Test
+	public void TestOne() {
+		driver.navigate().to(webUrl);
+		System.out.println("Test 1 title is "+driver.getTitle());
+	}
+	
+	@Test
+	public void TestTwo() {
+		driver.navigate().to(webUrl);
+		System.out.println("Test 2 title is "+driver.getTitle());
+	}
+	
+	@Test
+	public void TestThree() {
+		driver.navigate().to(webUrl);
+		System.out.println("Test 3 title is "+driver.getTitle());
+		Assert.assertEquals("Expected title", driver.getTitle());
+	}
+	
+	@AfterMethod
+	public void quit(ITestResult result) throws JiraException {
+		driver.quit();
+		//if test case fails then log the defect in JIRA
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+			BasicCredentials creds = new BasicCredentials("admin", "admin");
+			JiraClient jira = new JiraClient("http://localhost:8081", creds);
+			Issue issueName = jira.createIssue("AUT", "Bug").field(Field.SUMMARY, result.getMethod().getMethodName() +"is failed due to: "+ result.getThrowable().toString()).field(Field.DESCRIPTION, "get the description").execute();
+			System.out.println("Issue is created in JIRA with issue key: "+issueName.getKey());
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
